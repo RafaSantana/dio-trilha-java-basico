8b212eb3caf8563b0fdc9a2596826de842bee069
RafaSantana <rafaelsantana.tech@gmail.com>
2023-10-02 14:31:41 -0300

    implementando sistema Smart TV

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,61 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal(int canalEscolhido) {
+        if((canalEscolhido <= 0) || (canalEscolhido > 50)) {
+            canalEscolhido = 1;
+            System.out.println("Canal Inválido, tente outro...");
+        }else {
+            canal = canalEscolhido;
+        }
+    }
+
+    public void aumentarCanal() {
+        if(canal >= 50) {
+            canal = 1;
+            System.out.println("Canal está no máximo");
+        }else {
+            canal++;
+            System.out.println("Aumentando canal para: " + canal);
+        }
+    }
+
+        public void diminuirCanal() {
+            if(canal <= 1) {
+                canal = 50;
+                System.out.println("Canal está no mínimo");
+            }else {
+                canal--;
+                System.out.println("Diminuindo canal para: " + canal);
+            }
+    }
+
+    public void aumentarVolume() {
+        if(volume >= 50) {
+            volume = 50;
+            System.out.println("Volume está no máximo");
+        }else {
+            volume++;
+            System.out.println("Aumentando volume para: " + volume);
+        }
+    }
+
+    public void diminuirVolume() {
+        if(volume <= 0) {
+            volume = 0;
+            System.out.println("Volume está no mínimo");
+        }else {
+            volume--;
+            System.out.println("Diminuindo volume para: " + volume);
+        }
+    }
+
+    public void ligar() {
+        ligada = true;
+    }
+    public void desligar() {
+        ligada = false;
+    }
+}
