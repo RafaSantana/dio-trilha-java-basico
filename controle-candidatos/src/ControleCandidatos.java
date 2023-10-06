ab5119ed604329c436c2f182f79794e9b1c72bd9
RafaSantana <rafaelsantana.tech@gmail.com>
2023-10-06 17:03:56 -0300

    Implantados 1º e 2º case e aplicado POO

---
+++ controle-candidatos/src/ControleCandidatos.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ControleCandidatos {
+    public static void main(String[] args) throws Exception {
+        List<Candidato> candidatos = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quantos candidatos serão chamados para entrevista? ");
+        Integer quantidadeEntrevistados = sc.nextInt();
+        sc.close();
+
+        candidatos.add(new Candidato("FELIPE"));
+        candidatos.add(new Candidato("MÁRCIA"));
+        candidatos.add(new Candidato("JULIA"));
+        candidatos.add(new Candidato("PAULO"));
+        candidatos.add(new Candidato("AUGUSTO"));
+        candidatos.add(new Candidato("MÔNICA"));
+        candidatos.add(new Candidato("FABRÍCIO"));
+        candidatos.add(new Candidato("MIRELA"));
+        candidatos.add(new Candidato("DANIELA"));
+        candidatos.add(new Candidato("JORGE"));
+
+        EscolhaCandidatos.selecionarCandidatos(candidatos, quantidadeEntrevistados);
+
+    }
+
+    class EscolhaCandidatos {
+        static final Double SALARIOBASE = 2000.00;
+
+        static void selecionarCandidatos(List<Candidato> candidatos, Integer quantidadeEntrevistados) {
+            Integer contCandidatosAprovados = 0;
+
+            for (Candidato candidato : candidatos) {
+                if (candidato.getSalarioPretendido() <= SALARIOBASE) {
+                    System.out.println("Candidato " + candidato.getNomeCandidato() + " Qualificado"
+                            + candidato.getSalarioPretendido());
+                    contCandidatosAprovados++;
+                    if (contCandidatosAprovados.equals(quantidadeEntrevistados)) {
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        static void avaliarPretensaoSalario(List<Candidato> candidatos) {
+
+            for (Candidato candidato : candidatos) {
+                if (candidato.getSalarioPretendido() < SALARIOBASE) {
+                    System.out.println("LIGAR PARA O CANDIDATO");
+                } else if (candidato.getSalarioPretendido().equals(SALARIOBASE)) {
+                    System.out.println("LIGAR PARA O CANDIDATO, COM CONTRA PROPOSTA");
+                } else {
+                    System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS");
+                }
+            }
+
+        }
+    }
+
+    static class Candidato {
+        private Double salarioPretendido = 0.0;
+        private String nomeCandidato;
+
+        public Candidato(String nomeCandidato) {
+            this.nomeCandidato = nomeCandidato;
+            this.salarioPretendido = salarioPretendido();
+        }
+
+        Double salarioPretendido() {
+            return ThreadLocalRandom.current().nextDouble(1800, 2200);
+        }
+
+        public Double getSalarioPretendido() {
+            return this.salarioPretendido;
+        }
+
+        public String getNomeCandidato() {
+            return this.nomeCandidato;
+        }
+    }
+
+}
